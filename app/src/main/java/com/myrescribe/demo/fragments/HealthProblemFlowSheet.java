b4c0fc7ae198c419de6656cc5c18e3931e63700f
ganeshshirole <ganesh.shirole@scorgtechnologies.com>
2017-08-14 16:51:42 +0530

    3rd snd 4th form

--- app/src/main/java/com/myrescribe/demo/fragments/HealthProblemFlowSheet.java
+++ app/src/main/java/com/myrescribe/demo/fragments/HealthProblemFlowSheet.java
@@ -7,14 +7,14 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.myrescribe.demo.R;
 
 public class HealthProblemFlowSheet extends Fragment {
 
-    public HealthProblemFlowSheet() {
-        // Required empty public constructor
-    }
+   Spinner mHistorySpinner;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -27,7 +27,15 @@ public class HealthProblemFlowSheet extends Fragment {
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_health_problem_flow_sheet, container, false);
 
+        mHistorySpinner = (Spinner) rootView.findViewById(R.id.historySpinner);
 
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getContext(),
+                R.array.history_array, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        mHistorySpinner.setAdapter(adapter);
 
         return rootView;
     }
